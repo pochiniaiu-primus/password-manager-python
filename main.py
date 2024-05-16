91d1a7c15734785c6564d14803452bc16992d5fb
Serhii Honchar <serhii.honchar.java@gmail.com>
2024-05-16 21:36:11 +0200

    Save data to the file.

--- main.py
+++ main.py
@@ -2,6 +2,17 @@ from tkinter import *
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+def save():
+    website = entry_website.get()
+    email = entry_email_username.get()
+    password = entry_pass.get()
+    with open("data.txt", "a") as data_file:
+        data_file.write(f"{website} | {email} | {password}\n")
+        entry_website.delete(0, 'end')
+        entry_email_username.delete(0, 'end')
+        entry_pass.delete(0, 'end')
+
+        data_file.close()
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -23,6 +34,7 @@ pass_label = Label(text="Password:")
 pass_label.grid(column=0, row=3)
 
 entry_website = Entry()
+entry_website.focus()
 entry_website.insert(END, string="")
 entry_website.grid(column=1, row=1, columnspan=2, sticky="EW")
 
@@ -37,7 +49,7 @@ entry_pass.grid(column=1, row=3, sticky="EW")
 generate_password_button = Button(text="Generate Password")
 generate_password_button.grid(column=2, row=3, sticky="EW")
 
-add_button = Button(text="Add", width=35)
+add_button = Button(text="Add", width=35, command=save)
 add_button.grid(column=1, row=4, columnspan=2, sticky="EW")
 
 window.mainloop()
